26a0cdb59b1fa47390a93dbcefb4f2fd6be3340d
migorecki <115470105+migorecki@users.noreply.github.com>
2022-11-07 13:29:41 +0100

    Add files via upload

--- mzero.cpp
+++ mzero.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
+	int x;
+	cout<<"wybierz co chcesz zrobiæ: "<<endl;
+	cout<<"oblicznie funkcji liniowej wybierz 1"<<endl;
+	cout<<"oblicznie funkcji kwadratowej Ax+by+C=0 wybierz 2"<<endl;
+	cin>>x;
+	
+	if(x==1)
+	{
+	cout<<"funkcja liniowa"<<endl;
+		
+		{
 	double x;
 	double b;
 	cout<<"podaj wspólczynnik kierunkowy a"<<endl;
@@ -22,7 +35,26 @@ int main()
 		}
 	else
 	{
-		cout<<"miejsca zerowe fubnckji"<<(-x/b);	
-	}	
+		cout<<"miejsca zerowe fubnckji"<<(-x/b)<<endl;	
+	}
+	
+}		
+}
+else
+{
+	int a;
+	int b;
+	int c;
+	cout<<"fuckja kwadratowa Ax+by+C=0"<<endl; 
+	cout<<"podaj a b i c"<<endl;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cout << "funkcja kwadr"<< a <<"x + "<<b<<"y +"<<c<<"= 0 ma miejsce zerowe: ";	
+}	
+	
+	
+	
+system("pause");	
 }
 
